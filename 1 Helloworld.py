0f8e2126ad3583e040ab55e1336d0119edaaafe9
QADovah <30305297+QADovah@users.noreply.github.com>
2024-03-05 14:25:57 -0300

    carpeta correcta

---
+++ 1 Helloworld.py
@@ -0,0 +1,21 @@
+#Hola mundo
+#Hola mundo en py
+print("Hello World")
+
+
+"""
+ESTO ES UN COMENTARIO
+EN BLOQUE MULTIPLE
+"""
+
+
+#Imprime el tipo de dato de lo que se le envia:
+
+print(type("hola python")) # Tipo 'str'
+print(type(100.121212111111111111111111111)) # Tipo 'float'
+print(type(100)) # Tipo 'int'
+print(type(True)) # Tipo 'int'
+print(type(3 + 1j)) # Tipo 'int'
+
+#modificado
+#
---
+++ 2 Variables.py
@@ -0,0 +1,53 @@
+# Variables 
+# Con guiones bajos si hay espacios entre palabras, y todo en minusculas.
+
+
+my_variable = "Mi texto variable"
+print(my_variable)
+
+my_int_variable = 123
+print(my_int_variable)
+
+
+my_bool_variable = True
+print(my_bool_variable)
+
+
+#concatenación de variables
+print(my_bool_variable,",", my_int_variable,",",my_bool_variable)
+
+
+type(print(my_variable))
+#my_int_variable = 5
+#print(my_int_variable)
+#print(my_bool_variable, str(my_int_variable), my_variable)
+
+
+#Se imprime el tipo de dato print. Pero como es función, no devuelve tipo de dato valido. 
+print(type(print(my_variable)))
+
+
+#Imprime la cantidad de caracteres de la variable.
+print(len(my_variable))
+
+
+
+#Variables en una sola linea. 
+nombre, apellido, alias, edad = "Federico", "Astray", "Kala", 38
+print (nombre, apellido, alias, edad)
+print("mi nombre es", nombre, "Mi apellido es", apellido, "en la calle\n me dicen", alias, "Y tengo", edad ,"años")
+
+
+print(nombre)
+print(edad)
+
+nombre = input("Identifiques con el nombre")
+edad = input("Ingrese la edad")
+print("Gracias")
+
+
+address: str = "Sanchez 2011"
+
+print(address)
+
+#Sarasa modificado
